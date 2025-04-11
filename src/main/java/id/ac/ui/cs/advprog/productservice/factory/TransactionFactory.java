7446290654867cda89b134e162ffaa3957d77e12
sopigoo <shofinadhifa24@gmail.com>
2025-04-11 19:21:07 +0700

    [RED] Add transaction factory skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/productservice/factory/TransactionFactory.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.productservice.factory;
+
+import id.ac.ui.cs.advprog.productservice.model.Product;
+import id.ac.ui.cs.advprog.productservice.model.Transaction;
+import id.ac.ui.cs.advprog.productservice.model.builder.TransactionBuilder;
+
+import java.util.List;
+
+public class TransactionFactory {
+
+    public static Transaction create(String id, List<Product> products) {
+    }
+}
