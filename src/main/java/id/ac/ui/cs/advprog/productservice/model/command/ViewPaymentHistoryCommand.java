6471e766dc2558bb6934e5965b317cc17763de3e
mirfaknaufal <mirfakn@gmail.com>
2025-05-01 16:33:23 +0700

    [RED] Add tests for Payment model

--- src/main/java/id/ac/ui/cs/advprog/productservice/model/command/ViewPaymentHistoryCommand.java
+++ src/main/java/id/ac/ui/cs/advprog/productservice/model/command/ViewPaymentHistoryCommand.java
@@ -1,11 +1,12 @@
 package id.ac.ui.cs.advprog.productservice.model.command;
+import id.ac.ui.cs.advprog.productservice.service.PaymentServiceImpl;
 
 public class ViewPaymentHistoryCommand implements PaymentCommand {
 
-    private final PaymentService service;
-    private final Long customerId;
+    private final PaymentServiceImpl service;
+    private final String customerId;
 
-    public ViewPaymentHistoryCommand(PaymentService service, Long customerId) {
+    public ViewPaymentHistoryCommand(PaymentServiceImpl service, String customerId) {
         this.service = service;
         this.customerId = customerId;
     }
---
+++ src/main/java/id/ac/ui/cs/advprog/productservice/service/PaymentServiceImpl.java
@@ -0,0 +1,3 @@
+package id.ac.ui.cs.advprog.productservice.service;
+
+public class PaymentServiceImpl implements PaymentService {}
