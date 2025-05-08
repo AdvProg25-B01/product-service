e17cd36748801eb034ca406c68c6a9268b95cbc9
mirfaknaufal <mirfakn@gmail.com>
2025-05-08 15:43:00 +0700

    [REFACTOR] Removed State Design Pattern

--- src/main/java/id/ac/ui/cs/advprog/productservice/model/state/CicilanState.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.productservice.model.state;
-
-public class CicilanState implements PaymentState {
-    @Override
-    public void handle() {
-        System.out.println("Pembayaran dalam cicilan.");
-    }
-}
