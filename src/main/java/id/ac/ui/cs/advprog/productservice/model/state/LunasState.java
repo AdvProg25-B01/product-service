bba393b6266cbd5bcd829a2077ce57befe1ccfbe
mirfaknaufal <mirfakn@gmail.com>
2025-05-08 15:43:00 +0700

    [REFACTOR] Removed State Design Pattern

--- src/main/java/id/ac/ui/cs/advprog/productservice/model/state/LunasState.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.productservice.model.state;
-
-public class LunasState implements PaymentState {
-    @Override
-    public void handle() {
-        System.out.println("Pembayaran lunas.");
-    }
-}
--- src/main/java/id/ac/ui/cs/advprog/productservice/model/state/PaymentState.java
+++
@@ -1,5 +0,0 @@
-package id.ac.ui.cs.advprog.productservice.model.state;
-
-public interface PaymentState {
-    void handle();
-}
