a25891bd79323ab172860c6987f8537c08ea7ae8
sopigoo <shofinadhifa24@gmail.com>
2025-05-23 19:39:53 +0700

    [GREEN] Implement async config

--- src/main/java/id/ac/ui/cs/advprog/productservice/config/AsyncConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/productservice/config/AsyncConfig.java
@@ -13,6 +13,12 @@ public class AsyncConfig {
 
     @Bean(name = "customTaskExecutor")
     public Executor customTaskExecutor() {
-
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
     }
 }
