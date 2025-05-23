6b497400934bdddf270e710347a5f1f394a6c30f
sopigoo <shofinadhifa24@gmail.com>
2025-05-23 19:39:31 +0700

    [RED] Add async config skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/productservice/config/AsyncConfig.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "customTaskExecutor")
+    public Executor customTaskExecutor() {
+
+    }
+}
